6106c22477097f690bbd5635416d38bdde36fe8f
Uvejs Gjelaj <shiq.une1@gmail.com>
2023-05-03 20:42:19 +0200

    Done

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -153,6 +153,55 @@ function HomePage() {
             <div className="watch"> <h1 className="hwatch"> Shop activity trackers and <br/> smartwatches </h1> <img className="watchimg" src={watch} /></div>
         </div>
 
+
+        <div className="footer">
+            <div className="get">
+                <ul>
+                    <li className="h1"> Get to Know Us </li>
+                    <li> Careers </li>
+                    <li> Blog </li>
+                    <li> About Amazon </li>
+                    <li> Investor Relations </li>
+                    <li> Amazon Devices </li>
+                    <li> Amazon Science </li> 
+                </ul>
+            </div>
+            <div className="make">
+                <ul>
+                    <li className="h1"> Make Money with Us </li>
+                    <li> Sell products on Amazon </li>
+                    <li> Sell on Amazon Business </li>
+                    <li> Sell apps on Amazon </li>
+                    <li> Become an Affiliate </li>
+                    <li> Advertise Your Products </li>
+                    <li> Self-Publish with Us </li> 
+                    <li> Host an Amazon Hub </li>
+                    <li> › See More Make Money with Us </li>
+                </ul>
+            </div>
+            <div className="amazon">
+                <ul>
+                    <li className="h1"> Amazon Payment Products </li>
+                    <li> Amazon Business Card </li>
+                    <li> Shop with Points </li>
+                    <li> Reload Your Balance </li>
+                    <li> Amazon Currency Converter </li>
+                </ul>
+            </div>
+            <div className="amazon">
+                <ul>
+                    <li className="h1"> Amazon Payment Products </li>
+                    <li> Amazon and COVID-19 </li>
+                    <li> Your Account </li>
+                    <li> Your Orders </li>
+                    <li> Shipping Rates & Policies </li>
+                    <li> Returns & Replacements </li>
+                    <li> Manage Your Content and Devices </li>
+                    <li> Amazon Assistant </li>
+                    <li> Help </li>
+                </ul>
+            </div>
+        </div>
         </>
     )
 }
